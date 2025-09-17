9d3a9dbbfb6179eef977664a55e307449cd76928
Wojciech Grochecki <wojciech5522@gmail.com>
2025-09-17 11:43:06 +0200

    configure dependencies

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { Routes } from "react-router";
+import { TypographyH1 } from "./components/typography/typographyH1";
+
+function App() {
+  return <TypographyH1>ssdfkl//slkdfj</TypographyH1>;
+}
+
+export default App;
