72bf70dde732b1a2473ca8b8ce68e3a2782377d7
Wojciech Grochecki <wojciech5522@gmail.com>
2025-09-18 12:41:23 +0200

    create login and register pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
-import { Routes } from "react-router";
-import { TypographyH1 } from "./components/typography/typographyH1";
+import { Routes, Route, Navigate } from "react-router";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage.tsx";
+import HomePage from "./pages/HomePage.tsx";
+
+const isAuthenticated = () => false;
+
+function ProtectedRoute({ children }: { children: React.ReactNode }) {
+  return isAuthenticated() ? children : <Navigate to="/login" replace />;
+}
 
 function App() {
-  return <TypographyH1>ssdfkl//slkdfj</TypographyH1>;
+  return (
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <HomePage />
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,4 @@
+function HomePage() {
+  return <div>Home Page (Protected)</div>;
+}
+export default HomePage;
