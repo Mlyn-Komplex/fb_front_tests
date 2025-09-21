bb9171d9bdc4299d1f8a408cda8d3fcda9649b07
Wojciech Grochecki <wojciech5522@gmail.com>
2025-09-21 13:42:20 +0200

    add template for authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
-import { Routes, Route, Navigate } from "react-router";
+import { Routes, Route } from "react-router";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage.tsx";
 import HomePage from "./pages/HomePage.tsx";
 import NavLayout from "./layouts/NavLayout.tsx";
+import ProtectedRoute from "./auth/ProtectedRoute.tsx";
+import { AuthProvider } from "./auth/AuthContext.tsx";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route element={<NavLayout />}>
-        <Route index path="/" element={<HomePage />} />
-      </Route>
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route
+          element={
+            <ProtectedRoute>
+              <NavLayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index path="/" element={<HomePage />} />
+        </Route>
+      </Routes>
+    </AuthProvider>
   );
 }
 
