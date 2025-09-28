2f6cd0f8f11a89a9820ff851b6eb383464a418d3
Wojciech Grochecki <wojciech5522@gmail.com>
2025-09-28 09:51:01 +0200

    configure test environment

---
+++ src/tests/globals.d.ts
@@ -0,0 +1,17 @@
+/// <reference types="vitest/globals" />
+
+// Ensure global test functions are available
+declare global {
+  const describe: typeof import("vitest").describe;
+  const it: typeof import("vitest").it;
+  const test: typeof import("vitest").test;
+  const expect: typeof import("vitest").expect;
+  const beforeEach: typeof import("vitest").beforeEach;
+  const afterEach: typeof import("vitest").afterEach;
+  const beforeAll: typeof import("vitest").beforeAll;
+  const afterAll: typeof import("vitest").afterAll;
+  const vi: typeof import("vitest").vi;
+  const vitest: typeof import("vitest").vitest;
+}
+
+export {};
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,51 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   server: { open: true },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  test: {
+    globals: true, // allows using test(), expect(), etc. without imports
+    environment: "jsdom", // simulates browser APIs (good for React/Vue testing)
+    setupFiles: "./src/tests/setupTests.ts", // optional, for global test setup
+    include: ["src/**/*.{test,spec}.{js,ts,jsx,tsx}"], // which files to test
+    exclude: [
+      "node_modules",
+      "dist",
+      "src/components/ui",
+      "src/tests",
+      "src/main.tsx",
+    ], // files/folders to skip
+    typecheck: {
+      tsconfig: "./tsconfig.test.json",
+    },
+    coverage: {
+      provider: "v8", // coverage engine (fast + no deps)
+      reporter: ["text", "html"], // generate text + HTML reports
+      all: true, // include all files in coverage (not only tested files)
+      exclude: [
+        "node_modules/**",
+        "dist/**",
+        "src/tests/**",
+        "src/main.tsx",
+        "src/components/ui/**",
+        "**/*.test.{ts,tsx,js,jsx}",
+        "**/*.d.ts",
+        "vite.config.ts",
+        "vitest.config.ts",
+        "eslint.config.js",
+        "tailwind.config.js",
+        "src/components/typography/**",
+        "src/layouts/**",
+      ],
     },
   },
 });
